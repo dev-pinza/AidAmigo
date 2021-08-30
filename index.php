44e5fd4ac30e873a655a99612836d1e69fdebd88
dev-pinza <pinzastudio2020@gmail.com>
2021-08-30 12:56:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ include("core/functions/db/connect.php");
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
-		<meta name="description" content="description"/>
-		<meta name="keywords" content="keywords"/>
+		<meta name="description" content="Aid Amigo is an African for-profit crowdfunding platform that allows people to raise money for events ranging from life events such as celebrations and graduations to challenging circumstances like accidents and illnesses.">
+		<meta name="keywords" content="Aid Amigo, African, Crowdfunding, Investment, Money, People, Celebration, Illnesses, Accident, Education, Payment, Graduation">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
 		<link rel="shortcut icon" href="assets/img/logo/AIDAMIGO_SVG.svg"/>
